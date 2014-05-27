5b85fae1e00a22f9b9dc3dd9ce9fd6eeef90a7da
aandrewjeski <aandrewjeski@gmail.com>
2014-05-26 22:49:35 -0400

    Final Commit! Lots of changes

--- server.rb
+++ server.rb
@@ -10,48 +10,45 @@ def read_file
   scores
 end
 
-def load_teams
-  teams = []
-  CSV.foreach("teams.csv", headers: true, header_converters: :symbol) do |row|
-    teams << row.to_hash
-  end
-  teams
-end
-
 get '/' do
-  @team_name = load_teams
   @scores = read_file
   erb :index
 end
 
 get '/:leaderboard' do
   @scores = read_file
-  @home_win = []
-  @home_loss = []
-  @away_win = []
-  @away_loss = []
-  @ranking = []
-  @scores.each do |instance|
-    if instance[:home_score] > instance[:away_score]
-      @home_win << instance
-    elsif instance[:home_score] < instance[:away_score]
-      @home_loss << instance
+  @outcome = {}
+
+  @scores.each do |row|
+    unless @outcome.has_key?(row[:home_team])
+      @outcome[row[:home_team]] = {"win" => 0, "loss" => 0}
+    end
+    unless @outcome.has_key?(row[:away_team])
+      @outcome[row[:away_team]] = {"win" => 0, "loss" => 0}
     end
   end
-  @scores.each do |instance|
-    if instance[:away_score] > instance[:home_score]
-      @away_win << instance
-    elsif instance[:away_score] < instance[:home_score]
-      @away_loss << instance
+  @outcome
+
+  @scores.each do |row|
+    if row[:home_score] > row[:away_score]
+      @outcome[row[:home_team]]["win"] += 1
+      @outcome[row[:away_team]]["loss"] += 1
+    elsif row[:home_score] < row[:away_score]
+      @outcome[row[:away_team]]["win"] += 1
+      @outcome[row[:home_team]]["loss"] += 1
     end
   end
 
-  binding.pry
+  def leaderboard(place)
+    place.sort_by {|team, outcome| outcome["loss"]}
+  end
+
+  @outcome
+  @ranking = leaderboard(@outcome)
   erb :leaderboard
 end
 
-get '/:teams/:team_name' do
-@scores = load_teams
+get '/teams/:team_name' do
 
 erb :teams
 end
